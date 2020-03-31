1220e7994051cd66b4aa43833d8fca340b2cc02f
Stepan Snigur <snigur-stepan@mail.ru>
2020-03-31 13:16:17 +0300

    Add input placeholder

--- src/components/PostsSearch/PostsSearch.js
+++ src/components/PostsSearch/PostsSearch.js
@@ -5,7 +5,7 @@ import './PostSearch.css'
 const PostsSearch = ({ onChangePostSearchTerm }) => {
   return (
     <div className="posts-search">
-      <input className="posts-search__input" onChange={onChangePostSearchTerm} type="text"/>
+      <input className="posts-search__input" placeholder="Введите название поста" onChange={onChangePostSearchTerm} type="text"/>
       <button className="posts-search__button">Search by tags</button>
     </div>
   )
