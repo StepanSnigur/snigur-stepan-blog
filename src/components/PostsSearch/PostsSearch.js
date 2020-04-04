14bb012a81dbffea3a22c6d0ebdd70b0b0ef5046
Stepan Snigur <snigur-stepan@mail.ru>
2020-04-04 13:33:59 +0300

    Remove search button

--- src/components/PostsSearch/PostsSearch.js
+++ src/components/PostsSearch/PostsSearch.js
@@ -7,7 +7,6 @@ const PostsSearch = ({ onChangePostSearchTerm }) => {
   return (
     <div className="posts-search">
       <input className="posts-search__input" placeholder="Введите название поста" onChange={onChangePostSearchTerm} type="text"/>
-      <button className="posts-search__button">Search by tags</button>
     </div>
   )
 }
