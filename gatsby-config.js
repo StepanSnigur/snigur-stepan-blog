6fe2005e3d357743c0b7215c8db5431e1189c480
Stepan Snigur <snigur-stepan@mail.ru>
2020-04-05 12:33:46 +0300

    Change short name

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `snigur-stepan-blog`,
-        short_name: `st-blog`,
+        short_name: `snigur stepan blog`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
